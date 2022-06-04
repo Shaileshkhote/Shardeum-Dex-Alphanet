4fcf820f124891c3581f4cd1ec4e6b9179c728ba
Shailesh K <shaileshkhote@gmail.com>
2022-06-04 08:26:05 +0530

    fix: manifest logo and typo changes

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,22 @@
+import { MenuEntry } from '0xshm-uikit-v2'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/swap',
+  },
+
+  {
+    label: 'Swap',
+    icon: 'TradeIcon',
+    href: '/swap',
+  },
+  {
+    label: 'Add/Remove Liquidity',
+    icon: 'TradeIcon',
+    href: '/pool',
+  }
+]
+
+export default config
